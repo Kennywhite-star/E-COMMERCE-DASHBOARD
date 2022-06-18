c22b642b061a89db098c1e7a9a94d3fe41aecf38
KEHINDE OLANIYAN <64592116+Kennywhite-star@users.noreply.github.com>
2022-06-18 18:41:23 +0100

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,37 @@
+const Orders = [
+    {
+        productName: 'Foldabe Mini Drone',
+        ProductNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        ProductNumber: '36787',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        ProductNumber: '49675',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        ProductNumber: '96995',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+
+    },
+    {
+        productName: 'GPS 4K Drone',
+        ProductNumber: '22456',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+
+    }
+]
